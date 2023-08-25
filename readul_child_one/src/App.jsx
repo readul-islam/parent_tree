3557b1e9972e39de63ea2b6afc9884271a17fd84
readul-islam <developer.readul@gmail.com>
2023-08-25 08:04:21 +0600

    u

--- readul_child_one/src/App.jsx
+++ readul_child_one/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        changes from readul sub repository
+        changes from readul islam repository
       </p>
     </>
   )
